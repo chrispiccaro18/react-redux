aba0129c1b5d03f616833074cb4a7a2718a75d6a
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-22 16:53:06 -0700

    testing lunch reducer

---
+++ src/reducers/lunchReducer.test.js
@@ -0,0 +1,46 @@
+import reducer from './lunchReducer';
+import { addDrink, removeDrink, addChips } from '../actions/lunchActions';
+
+describe('lunch reducer', () => {
+  it('can handle adding a drink', () => {
+    const initialState = {
+      drink: [],
+      chips: ['salt & pepper'],
+      sandwich: ['jam']
+    };
+
+    expect(reducer(initialState, addDrink('coconut water'))).toEqual({
+      drink: ['coconut water'],
+      chips: ['salt & pepper'],
+      sandwich: ['jam']
+    });
+  });
+  
+  it('can handle removing a drink', () => {
+    const initialState = {
+      drink: ['coconut water'],
+      chips: ['salt & pepper'],
+      sandwich: ['jam']
+    };
+
+    expect(reducer(initialState, removeDrink('coconut water'))).toEqual({
+      drink: [],
+      chips: ['salt & pepper'],
+      sandwich: ['jam']
+    });
+  });
+  
+  it('can handle adding chips', () => {
+    const initialState = {
+      drink: ['coconut water'],
+      chips: ['salt & pepper'],
+      sandwich: ['jam']
+    };
+
+    expect(reducer(initialState, addChips('salt & vin'))).toEqual({
+      drink: ['coconut water'],
+      chips: ['salt & pepper', 'salt & vin'],
+      sandwich: ['jam']
+    });
+  });
+});
