d2c4461e9855c10a69e4c4b84061351e5f159efd
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-23 15:49:14 -0700

    refactored comment state

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -1,27 +1,25 @@
 import { CREATE_COMMENT, DELETE_COMMENT } from '../actions/commentActions';
 
-const initialState = {
-  comments: {}
-};
+const initialState = {};
 
 export default function commentReducer(state = initialState, action) {
   const { type, payload } = action;
   switch(type) {
     case CREATE_COMMENT: {
       const { postIndex, body } = payload;
-      if(state.comments[postIndex]) {
-        const updatedComments = [...state.comments[postIndex], body];
-        return { comments: { ...state.comments, [postIndex]: updatedComments } };
+      if(state[postIndex]) {
+        const updatedComments = [...state[postIndex], body];
+        return { ...state, [postIndex]: updatedComments };
       } 
-      return { comments: { ...state.comments, [postIndex]: [body] } };
+      return { ...state, [postIndex]: [body] };
     }
     case DELETE_COMMENT: {
       const { postIndex, commentIndex } = payload;
       return { 
-        comments: { ...state.comments, [postIndex]: [
-          ...state.comments[postIndex].slice(0, commentIndex),
-          ...state.comments[postIndex].slice(commentIndex + 1)
-        ] } 
+        ...state, [postIndex]: [
+          ...state[postIndex].slice(0, commentIndex),
+          ...state[postIndex].slice(commentIndex + 1)
+        ] 
       };
     }
     default:
--- src/reducers/commentReducer.test.js
+++ src/reducers/commentReducer.test.js
@@ -4,68 +4,50 @@ import { createComment, deleteComment } from '../actions/commentActions';
 describe('comment reducer tests', () => {
   it('can create a comment', () => {
     const initialState = {
-      comments: {
-        0: ['already comment body'],
-        1: ['another comment']
-      }
+      0: ['already comment body'],
+      1: ['another comment']
     };
 
     expect(commentReducer(initialState, createComment(0, 'added comment'))).toEqual({
-      comments: {
-        0: ['already comment body', 'added comment'],
-        1: ['another comment']
-      }
+      0: ['already comment body', 'added comment'],
+      1: ['another comment']
     });
     expect(initialState).toEqual({
-      comments: {
-        0: ['already comment body'],
-        1: ['another comment']
-      }
+      0: ['already comment body'],
+      1: ['another comment']
     });
   });
   
   it('can create a comment on a post that has no comments', () => {
     const initialState = {
-      comments: {
-        0: ['already comment body'],
-        1: ['another comment']
-      }
+      0: ['already comment body'],
+      1: ['another comment']
     };
 
     expect(commentReducer(initialState, createComment(2, 'added comment'))).toEqual({
-      comments: {
-        0: ['already comment body'],
-        1: ['another comment'],
-        2: ['added comment']
-      }
+      0: ['already comment body'],
+      1: ['another comment'],
+      2: ['added comment']
     });
     expect(initialState).toEqual({
-      comments: {
-        0: ['already comment body'],
-        1: ['another comment']
-      }
+      0: ['already comment body'],
+      1: ['another comment']
     });
   });
   
   it('can delete a comment', () => {
     const initialState = {
-      comments: {
-        0: ['already comment body'],
-        1: ['another comment', 'to be deleted']
-      }
+      0: ['already comment body'],
+      1: ['another comment', 'to be deleted']
     };
 
     expect(commentReducer(initialState, deleteComment(1, 1))).toEqual({
-      comments: {
-        0: ['already comment body'],
-        1: ['another comment']
-      }
+      0: ['already comment body'],
+      1: ['another comment']
     });
     expect(initialState).toEqual({
-      comments: {
-        0: ['already comment body'],
-        1: ['another comment', 'to be deleted']
-      }
+      0: ['already comment body'],
+      1: ['another comment', 'to be deleted']
     });
   });
 });
