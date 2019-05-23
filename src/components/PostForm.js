7b5de288a1c699934c18bfbb4d39584893e3cd95
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-23 14:44:03 -0700

    post form written

---
+++ src/components/PostForm.js
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class PostForm extends PureComponent {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired
+  }
+
+  state = {
+    title: '',
+    body: ''
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value
+    });
+  }
+
+  render() {
+    const { title, body } = this.state;
+    return (
+      <form onSubmit={this.props.onSubmit}>
+        <input type="text" name="title" value={title} onChange={this.handleChange} />
+        <input type="text" name="body" value={body} onChange={this.handleChange} />
+        <button>Create</button>
+      </form>
+    );
+  }
+}
