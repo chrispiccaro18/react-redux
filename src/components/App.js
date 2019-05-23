78e6f9368fc6cf5c7f5426f4a773070a0adeeb05
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-23 15:05:01 -0700

    will it render?

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import Posts from './Posts';
-import PostForm from './PostForm';
-
-const posts = [
-  { title: 'first post', body: 'lorem ipsum aupsum' },
-  { title: 'second post', body: 'lorem ipsum bipsum' },
-  { title: 'third post', body: 'lorem ipsum dipsum' }
-];
-
-const testOnSubmit = event => {
-  event.preventDefault();
-  console.log('submit', event.target);
-};
+import CreatePost from '../containers/CreatePost';
+import AllPosts from '../containers/AllPosts';
 
 export default function App() {
   return (
     <>
-      <PostForm onSubmit={testOnSubmit} />
-      <Posts posts={posts} />
+      <CreatePost />
+      <AllPosts />
     </>
   );
 }
