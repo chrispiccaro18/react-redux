9268b0b7a6f4f503c830d9bc0623d607efaa5f95
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-23 09:59:58 -0700

    changed name and fixed linting errors

--- src/actions/postActions.test.js
+++ src/actions/postActions.test.js
@@ -1,9 +1,9 @@
-import { createPost, CREATE_POST, deletePost, DELETE_POST, updatePost, UPDATE_POST } from "./postActions";
+import { createPost, CREATE_POST, deletePost, DELETE_POST, updatePost, UPDATE_POST } from './postActions';
 
 describe('post actions tests', () => {
   it('can create a post', () => {
     const testTitle = 'test';
-    const testBody = 'test test test'
+    const testBody = 'test test test';
     expect(createPost(testTitle, testBody)).toEqual({
       type: CREATE_POST,
       payload: { title: testTitle, body: testBody }
@@ -19,7 +19,7 @@ describe('post actions tests', () => {
   });
   
   it('can update a post', () => {
-    const index = 1
+    const index = 1;
     const testBody = 'test test test';
     expect(updatePost(index, testBody)).toEqual({
       type: UPDATE_POST,
