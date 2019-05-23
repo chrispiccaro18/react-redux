2ec1cd27a5e411c06d5b556a9186e182047d776c
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-22 17:21:01 -0700

    update post handled

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { CREATE_POST, DELETE_POST } from '../actions/postActions';
+import { CREATE_POST, DELETE_POST, UPDATE_POST } from '../actions/postActions';
 
 const initialState = {
   posts: []
@@ -11,6 +11,9 @@ export default function postReducer(state = initialState, action) {
       return { posts: [...state.posts, payload] };
     case DELETE_POST:
       return { posts: state.posts.filter(post => post.title !== payload.title) };
+    case UPDATE_POST:
+      state.posts[payload.index].body = payload.body;
+      return { posts: [...state.posts] };
     default:
       return state;
   }
--- src/reducers/postReducer.test.js
+++ src/reducers/postReducer.test.js
@@ -1,5 +1,5 @@
 import postReducer from './postReducer';
-import { createPost, deletePost } from '../actions/postActions';
+import { createPost, deletePost, updatePost } from '../actions/postActions';
 
 describe('post reducer tests', () => {
   it('can create a post', () => {
@@ -49,6 +49,45 @@ describe('post reducer tests', () => {
         {
           title: 'Also Not My Post',
           body: 'lorem ipsum'
+        }
+      ]
+    });
+  });
+  
+  it('can update a post', () => {
+    const initialState = {
+      posts: [
+        {
+          title: 'Also Not My Post',
+          body: 'lorem ipsum'
+        },
+        {
+          title: 'Not My Post',
+          body: 'lorem ipsum'
+        },
+        {
+          title: 'My Post',
+          body: 'this should change'
+        }
+      ]
+    };
+
+    const index = 2;
+    const newBody = 'here i am to save the day';
+
+    expect(postReducer(initialState, updatePost(index, newBody))).toEqual({
+      posts: [
+        {
+          title: 'Also Not My Post',
+          body: 'lorem ipsum'
+        },
+        {
+          title: 'Not My Post',
+          body: 'lorem ipsum'
+        },
+        {
+          title: 'My Post',
+          body: 'here i am to save the day'
         }
       ]
     });
