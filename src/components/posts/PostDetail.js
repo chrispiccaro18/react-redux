6b146517349cc1c2c24ab77bf110f22464bff372
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-24 10:30:25 -0700

    will it work?

--- src/components/posts/PostDetail.js
+++ src/components/posts/PostDetail.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CreateComment from '../../containers/comments/CreateComment';
+import AllComments from '../../containers/comments/AllComments';
 
 function PostDetail({ post }) {
   const { title, body } = post;
   return (
-    <>
+    <body>
       <section>
         <h2>{title}</h2>
         <p>{body}</p>
       </section>
-    </>
+      <section>
+        <CreateComment />
+      </section>
+      <section>
+        <AllComments />
+      </section>
+    </body>
   );
 }
 
