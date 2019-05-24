3fb0b52abbe5def962783e65b31f9af90071d3f4
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-23 17:52:13 -0700

    added a header

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import PostById from '../containers/PostById';
+import Header from './Header';
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route path="/post/:id" component={PostById}/>
         <Route path="/" component={Landing}/>
